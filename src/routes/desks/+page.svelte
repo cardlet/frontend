8d598f9ba4f40c3c513f4caf8206a89e1cd8253a
Ounzy <lenas.fehler@web.de>
2022-10-09 10:05:33 +0200

    added desks add inout and button

---
+++ src/routes/desks/+page.svelte
@@ -0,0 +1,43 @@
+<svelte:head>
+	<title>desks</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+	<input type="text" id="deskname" placeholder="Name of your new desk">
+	<button name="" id="save">Save</button>
+
+</section>
+
+
+<style>
+	#deskname {
+		position: absolute;
+		align-items: center;
+		width: 14%;
+		left: 2%;
+		top: 10%;
+		display: flex;
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+		background-color: var(--color-bg-2);
+	}
+
+	#save {
+		position: absolute;
+		right: 2%;
+		top: 10%;
+		display: flex;
+		align-items: center;
+		background-color: var(--color-bg-2);
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+	}
+
+	#save:hover {
+		text-decoration: none;
+		filter: drop-shadow(2);
+	}
+
+</style>
